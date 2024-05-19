0d8405f39db43d2c81829c76ceb2b926ab74de55
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-20 01:19:51 +0700

    Using BackgroundTasks

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi import Path
+from fastapi import BackgroundTasks
+
+from typing import Annotated
 
 app = FastAPI()
+
+def write_notification(email: str, message=""):
+  with open("log.txt", mode="w") as email_file:
+    content = f"notification for {email}: {message}"
+    email_file.write(content)
 
 @app.get("/")
 async def home():
   return {"message": "hello"}
+
+# Using BackgroundTasks
+
+@app.post("/send_notification/{email}")
+async def send_notification(email: Annotated[str, Path()], background_tasks: BackgroundTasks):
+  background_tasks.add_task(write_notification, email = email, message = "some notification")
+  return {"message": "Notification sent in the background"}
